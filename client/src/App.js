b8e1a1d0ed0fb98a4aac5257f6682a5aea5d92cf
Jenn <jenn@Jenns-MacBook-Pro.local>
2019-07-11 17:54:53 -0700

    initial framework

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
